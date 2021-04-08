c50d3ce7c3649d2b9f5d957e5ec9c6a8e9405607
TheQuyen-vnpt <quyennt@vnpt-technology.vn>
2021-04-08 14:46:23 +0700

    Quyen

--- src/main/java/application/AuthenticationListener.java
+++ src/main/java/application/AuthenticationListener.java
@@ -110,7 +110,6 @@ public class AuthenticationListener implements ApplicationListener<ContextRefres
                         runRepositoy.save(run);
                     }
                 }
-
             }
         }
     }
